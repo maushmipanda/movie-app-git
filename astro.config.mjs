1f19dbbdda95bc62d7bec0cdcea328bde1a3f35b
Maushmi <maushmi.work@gmail.com>
2024-07-29 15:53:42 +0100

    add file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://maushmipanda.github.io',
+  site: 'https://maushmipanda/movieastro/.github.io',
+  base:  'movie-app-git',
 
 })
